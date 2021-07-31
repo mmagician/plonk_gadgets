d9960d77027f8ffb5531f2921da553b10b52e426
Marcin Górny <marcin.gorny.94@protonmail.com>
2021-07-31 15:14:19 +0200

    Be more explicit and consistent about circuit inputs
    - Fix inline comments
    - Only need one correct prover instance (and two different verifier circuits)

--- tests/set_gadgets_tests.rs
+++ tests/set_gadgets_tests.rs
@@ -12,12 +12,13 @@ use plonk_gadgets::{Error as GadgetError, SetGadgets::*};
 
 #[test]
 fn test_set_non_membership_gadget() -> Result<(), Error> {
-    // The circuit closure runs the is_not_zero fn and constraints the input to
-    // not be zero.
-    let circuit = |composer: &mut StandardComposer,
-                   set: Vec<BlsScalar>,
-                   value: BlsScalar|
-     -> Result<(), GadgetError> { set_non_membership_gadget(composer, set, value) };
+    // The circuit closure runs the set_non_membership gadget, which constraints the value to
+    // not be part of the set.
+    let circuit =
+        |composer: &mut StandardComposer,
+         set: Vec<BlsScalar>,
+         value: BlsScalar|
+         -> Result<(), GadgetError> { set_non_membership_gadget(composer, set, value) };
 
     // Generate Composer & Public Parameters
     let pub_params = PublicParameters::setup(1 << 8, &mut rand::thread_rng())?;
@@ -25,41 +26,32 @@ fn test_set_non_membership_gadget() -> Result<(), Error> {
     let set = vec![BlsScalar::from(1), BlsScalar::from(2)];
     let different_set = vec![BlsScalar::from(3), BlsScalar::from(4)];
 
-    // Value is part of the set, shouldn't be able to construct a circuit
+    // Value is part of the set, prover shouldn't be able to construct an incorrect circuit
     let mut prover = Prover::new(b"testing");
-    assert!(circuit(prover.mut_cs(), set.clone(), BlsScalar::from(2)).is_err());
-    // prover.clear_witness();
+    assert!(circuit(prover.mut_cs(), set.clone(), set[0].clone()).is_err());
 
     // Prover and verifier using different sets should fail
     // Prover constructs a correct circuit
     let mut prover = Prover::new(b"testing");
-    assert!(circuit(prover.mut_cs(), different_set.clone(), BlsScalar::from(5)).is_ok());
+    assert!(circuit(prover.mut_cs(), set.clone(), BlsScalar::from(5)).is_ok());
 
-    let mut pi = prover.mut_cs().construct_dense_pi_vec().clone();
-    // let mut pi: Vec<PublicInputValue> = set.into_iter().map(|x| x.into()).collect();
+    let pi = prover.mut_cs().construct_dense_pi_vec().clone();
     prover.preprocess(&ck)?;
     let proof = prover.prove(&ck)?;
 
     // Verification
     let mut verifier = Verifier::new(b"testing");
     // verifier can construct a correct circuit
-    assert!(circuit(verifier.mut_cs(), set.clone(), BlsScalar::zero()).is_ok());
+    assert!(circuit(verifier.mut_cs(), different_set.clone(), BlsScalar::zero()).is_ok());
     verifier.preprocess(&ck)?;
+    // But his set corresponds to a different set than prover's, so the proof won't be correct
     assert!(verifier.verify(&proof, &vk, &pi).is_err());
 
-    // Value & value assigned set correctly. This should pass.
-    // Proving
-    let mut prover = Prover::new(b"testing");
-    circuit(prover.mut_cs(), set.clone(), BlsScalar::from(7))
-        .map_err(|_| Error::BlsScalarMalformed)?;
-    pi = prover.mut_cs().construct_dense_pi_vec().clone();
-    let proof = prover.prove(&ck)?;
-
+    // Both prover and verifier using the same circuit. This should pass.
     let mut verifier = Verifier::new(b"testing");
     assert!(circuit(verifier.mut_cs(), set.clone(), BlsScalar::zero()).is_ok());
     verifier.preprocess(&ck)?;
     assert!(verifier.verify(&proof, &vk, &pi).is_ok());
-    // assert!(verifier.verify(&proof, &vk, &vec![BlsScalar::zero()]).is_ok());
 
     Ok(())
 }
